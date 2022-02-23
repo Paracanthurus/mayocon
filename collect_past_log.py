e54250e117f19bfef7e7b77dbeae4e1444696041
Paracanthurus <okubo@MacBook-Pro.local>
2022-02-24 02:21:49 +0900

    処理の順番を変更

--- collect_past_log.py
+++ collect_past_log.py
@@ -23,56 +23,61 @@ wait = WebDriverWait(driver = driver, timeout = 60)
 
 def display_All():
 	dropdown = driver.find_element(by = By.XPATH, value = '//*[@id="root"]/div/div[2]/div[6]/div/div/div[1]/div/div[1]/span')
-	dropdown.find_element(by = By.TAG_NAME, value = 'button').click()
-	time.sleep(0.3)
+	bottun = dropdown.find_element(by = By.TAG_NAME, value = 'button')
+	driver.execute_script("arguments[0].click();", bottun)
+	time.sleep(1)
 	ul = dropdown.find_element(by = By.TAG_NAME, value = 'ul')
 	li = ul.find_elements(by = By.TAG_NAME, value = 'li')
 	li[4].find_element(by = By.TAG_NAME, value = 'a').click()
 	wait.until(EC.presence_of_all_elements_located)
-	time.sleep(0.3)
+	time.sleep(1)
 
-def collect_past_log():
+def collect_contests_url():
+	url_list = []
 	driver.get('https://kenkoooo.com/atcoder/#/contest/recent')
 	wait.until(EC.presence_of_all_elements_located)
 	time.sleep(1)
 	display_All()
 	table =  driver.find_element(by = By.XPATH, value = '//*[@id="root"]/div/div[2]/div[6]/div/div/div[2]/div[2]/table/tbody')
-	tr = table.find_elements(by = By.TAG_NAME, value = 'tr')
-	time.sleep(0.3)
-	n = len(tr)
-	for i in range(n):
-		table =  driver.find_element(by = By.XPATH, value = '//*[@id="root"]/div/div[2]/div[6]/div/div/div[2]/div[2]/table/tbody')
-		tr = table.find_elements(by = By.TAG_NAME, value = 'tr')
-		tds = tr[i].find_elements(by = By.TAG_NAME, value = 'td')
+	trs = table.find_elements(by = By.TAG_NAME, value = 'tr')
+	for tr in trs:
+		tds = tr.find_elements(by = By.TAG_NAME, value = 'td')
 		title = tds[0].find_element(by = By.TAG_NAME, value = 'a').get_attribute("textContent")
+		href = tds[0].find_element(by = By.TAG_NAME, value = 'a').get_attribute("href")
 		title = title[7:]
 		print(title)
 		date = tds[3].find_element(by = By.TAG_NAME, value = 'div').get_attribute("textContent")
 		date = date.split(' ')[0]
 		print(date)
 		if title == contest_title:
-			tds[0].find_element(by = By.TAG_NAME, value = 'a').click()
-			wait.until(EC.presence_of_all_elements_located)
-			time.sleep(1)
-			file = open(S.Dir_path + '/past_log/' + date, 'w')
-			table_2 = driver.find_element(by = By.XPATH, value = '//*[@id="root"]/div/div[2]/div[6]/div[2]/div/table/tbody')
-			tr_2 = table_2.find_elements(by = By.TAG_NAME, value = 'tr')
-			for problem in tr_2:
-				tds_2 = problem.find_elements(by = By.TAG_NAME, value = 'td')
-				problem_url = tds_2[0].find_element(by = By.TAG_NAME, value = 'a').get_attribute('href')
-				problem_name = problem_url.split('/')[-1]
-				file.write(problem_name + '\n')
-			file.close()
-			driver.back()
-			wait.until(EC.presence_of_all_elements_located)
-			time.sleep(1)
-			display_All()
+			url_list.append((href, date))
+	return url_list
 
+def collect_past_log(url, date):
+	driver.get(url)
+	wait.until(EC.presence_of_all_elements_located)
+	time.sleep(1)
+	file = open(S.Dir_path + '/past_log/' + date, 'w')
+	table_2 = driver.find_element(by = By.XPATH, value = '//*[@id="root"]/div/div[2]/div[6]/div[2]/div/table/tbody')
+	tr_2 = table_2.find_elements(by = By.TAG_NAME, value = 'tr')
+	for problem in tr_2:
+		tds_2 = problem.find_elements(by = By.TAG_NAME, value = 'td')
+		problem_url = tds_2[0].find_element(by = By.TAG_NAME, value = 'a').get_attribute('href')
+		problem_name = problem_url.split('/')[-1]
+		file.write(problem_name + '\n')
+	file.close()
 
-if __name__ == '__main__':
+def main():
 	try:
-		collect_past_log()
+		url_list = collect_contests_url()
+		for (url, date) in url_list:
+			print("past_logに追加中（" + date + "）")
+			collect_past_log(url, date)
 	except Exception:
 		print(traceback.format_exc())
 	finally:
 		driver.close()
+
+
+if __name__ == '__main__':
+	main()
